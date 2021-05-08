1698b4da9266039581e158ecb8ef798bd60964fd
Carlos Azaustre <cazaustre@gmail.com>
2021-05-08 20:32:03 +0200

    add http methods

--- notes-server/index.js
+++ notes-server/index.js
@@ -31,19 +31,22 @@ app.use(express.json());
 app.use(express.static("build"));
 app.use(requestLogger);
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+app.get("/api/notes", (req, res, next) => {
+  Note.find({})
+    .then((notes) => res.json(notes))
+    .catch((error) => next(error));
 });
 
-app.get("/api/notes", (req, res) => {
-  Note.find({}).then((notes) => res.json(notes));
+app.get("/api/notes/:id", (req, res, next) => {
+  Note.findById(req.params.id)
+    .then((note) => {
+      if (note) res.json(note);
+      else res.status(404).end();
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/notes/:id", (req, res) => {
-  Note.findById(req.params.id).then((note) => res.json(note));
-});
-
-app.post("/api/notes", (req, res) => {
+app.post("/api/notes", (req, res, next) => {
   const body = req.body;
 
   if (!body.content || body.content === undefined) {
@@ -56,14 +59,31 @@ app.post("/api/notes", (req, res) => {
     date: new Date(),
   });
 
-  note.save().then((savedNote) => res.json(savedNote));
+  note
+    .save()
+    .then((savedNote) => res.json(savedNote))
+    .catch((error) => next(error));
 });
 
-app.delete("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  notes = notes.filter((note) => note.id !== id);
+app.put("/api/notes/:id", (req, res, next) => {
+  const body = req.body;
 
-  res.status(204).end();
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then((updatedNote) => res.json(updatedNote))
+    .catch((error) => next(error));
+});
+
+app.delete("/api/notes/:id", (req, res, next) => {
+  Note.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (req, res) => {
@@ -71,6 +91,18 @@ const unknownEndpoint = (req, res) => {
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error);
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

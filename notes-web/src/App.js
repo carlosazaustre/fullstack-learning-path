9606795a7e9bdf28614b473480b096ac9a00a604
Carlos Azaustre <cazaustre@gmail.com>
2021-05-08 14:05:36 +0200

    error notifications

--- notes-web/src/App.js
+++ notes-web/src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import noteService from "./services/notes";
 import { Note } from "./components/Note";
+import { Notification } from "./components/Notification";
 
 export const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
@@ -30,7 +32,10 @@ export const App = () => {
         setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
       })
       .catch((error) => {
-        alert(`the note '${note.content}' was already deleted from server`);
+        setErrorMessage(
+          `Note '${note.content}' was already deleted from server`
+        );
+        setTimeout(() => setErrorMessage(null), 5000);
         setNotes(notes.filter((n) => n.id !== id));
       });
   };
@@ -53,6 +58,7 @@ export const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
--- notes-web/src/components/Note.js
+++ notes-web/src/components/Note.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Note = ({ note, toggleImportance }) => {
   const label = note.imporant ? "make not important" : "make important";
   return (
-    <li>
+    <li className="note">
       {note.content}
       <button onClick={toggleImportance}>{label}</button>
     </li>

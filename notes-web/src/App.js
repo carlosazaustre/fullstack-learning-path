8758628e47129c53992b8764f7cf8e75d1f076ea
Carlos Azaustre <cazaustre@gmail.com>
2021-05-09 17:29:26 +0200

    feat(web): :sparkles: add login form

--- notes-web/src/App.js
+++ notes-web/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import noteService from "./services/notes";
+import loginService from "./services/login";
 import { Note } from "./components/Note";
 import { Notification } from "./components/Notification";
 import { Footer } from "./components/Footer";
@@ -9,6 +10,9 @@ export const App = () => {
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
@@ -20,6 +24,21 @@ export const App = () => {
 
   const handleNoteChange = (event) => {
     setNewNote(event.target.value);
+  };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
   };
 
   const toggleImportanceOf = (id) => {
@@ -54,10 +73,52 @@ export const App = () => {
     });
   };
 
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input type="text" value={newNote} onChange={handleNoteChange} />
+      <button type="submit">Save</button>
+    </form>
+  );
+
   return (
     <div>
-      <h1>Notes</h1>
+      <h1>Notes Fullstack App</h1>
       <Notification message={errorMessage} />
+
+      {user === null ? (
+        loginForm()
+      ) : (
+        <div>
+          <p>{user.name} logged-in</p>
+          {noteForm()}
+        </div>
+      )}
+
+      <h2>Notes</h2>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
@@ -72,10 +133,6 @@ export const App = () => {
           />
         ))}
       </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">Save</button>
-      </form>
       <Footer />
     </div>
   );
